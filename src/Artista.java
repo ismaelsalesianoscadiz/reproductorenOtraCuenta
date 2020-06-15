e5e8f34573ba6baa203f08c515f4e2447233799e
TorreISM <TorreISM@192.168.1.130>
2020-06-15 11:07:57 +0200

    Inicio Git examen

---
+++ src/Artista.java
@@ -0,0 +1,18 @@
+
+
+public class Artista {
+    String nombreArtista;
+
+    public Artista(String nombreArtista) {
+        this.nombreArtista = nombreArtista;
+    }
+
+    @Override
+    public String toString() {
+        return "Artista{" +
+                "nombreArtista='" + nombreArtista + '\'' +
+                '}';
+    }
+}
+
+
